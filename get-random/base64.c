9a7ec1e95fcb3f5fc624208bdc27a6039a9a7698
David Egan <david@carawebs.com>
2018-08-20 17:01:51 +0100

    Fix last char

--- get-random/base64.c
+++ get-random/base64.c
@@ -4,6 +4,7 @@
 void base64Encode(unsigned char *inputBuffer, unsigned char *b64Buffer, size_t inputLength)
 {
     size_t b64BufferLength = lenCharsBase64(inputLength);
+    printf("This function should output %lu base 64 characters.\n", b64BufferLength);
 
     // The first character
     // -------------------------------------------------------------------------
@@ -14,12 +15,12 @@ void base64Encode(unsigned char *inputBuffer, unsigned char *b64Buffer, size_t i
     b64Buffer[0] = encodingTable[lookupVal];                    // Get the b64 character
     size_t divider = 4;                                         // First run: set the divider for next char
 
-    // Loop to the penultimate character
+    // Loop to the penultimate character (assuming last char is a newline)
     // The last 2 bits of tmpByte is the last 2 bits of the first char
     // -------------------------------------------------------------------------
     size_t i = 1;
     size_t bufIndex = 1;
-    while (i <= inputLength) {
+    while (i < inputLength) {
         // Combine current char data with the previous (tmpByte)
         // ---------------------------------------------------------------------
         tmpByte <<= divider;                                        // Shift bits from previous iteration to the correct position
@@ -27,22 +28,29 @@ void base64Encode(unsigned char *inputBuffer, unsigned char *b64Buffer, size_t i
         unsigned char mostSigBits = inputBuffer[i] & mask;          // Collect the first `divider` bits of the current char
         mostSigBits >>= (8 - divider);                              // Shift to correct position
         b64Buffer[bufIndex] = encodingTable[tmpByte ^ mostSigBits]; // Add combined bytes to the return buffer
+        bufIndex++;
 
         // If the divider == 2, set the last 6 bits as a new radix 64 character
         // ---------------------------------------------------------------------
         if (divider == 2) {
-            b64Buffer[++bufIndex] = encodingTable[(inputBuffer[i] & (0xFF >> divider))];
-            tmpByte = 0; // Don't carry data forward
-        } else {
-            // mask to get the unused bits from the current character:
-            tmpByte = inputBuffer[i] & (0xFF >> divider);
+            b64Buffer[bufIndex] = encodingTable[(inputBuffer[i] & (0xFF >> divider))];
             bufIndex++;
+            tmpByte = 0; // In this case, don't carry data forward
+        } else {
+            // Carry forward the unused bits from the current character
+            tmpByte = inputBuffer[i] & (0xFF >> divider);   // Apply a logical & against a mask of the last bits
         }
 
         // Set divider for the next char
         // ---------------------------------------------------------------------
         divider = (divider - 2) ? divider - 2 : 6;
         i++;
+    }
+    // Add the remaining data from the tmpByte
+    b64Buffer[bufIndex] = encodingTable[tmpByte <<= divider];
+
+    while (bufIndex < b64BufferLength) {
+        b64Buffer[++bufIndex] = '=';
     }
     b64Buffer[bufIndex] = '\0';
 }
